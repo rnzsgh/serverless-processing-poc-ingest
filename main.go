4408ce57553b4f1d6b2ccf44c83c6ef4ed2cc5e7
rnzsgh <ryannitz@amazon.com>
2021-08-14 00:19:45 -0400

    Update

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	proto "github.com/golang/protobuf/proto"
+	model "github.com/rnzsgh/serverless-processing-poc-ingest/protob/model"
 )
 
 func handler(ctx context.Context, kinesisEvent events.KinesisEvent) error {
@@ -13,10 +15,20 @@ func handler(ctx context.Context, kinesisEvent events.KinesisEvent) error {
 	for _, record := range kinesisEvent.Records {
 
 		kinesisRecord := record.Kinesis
-		dataBytes := kinesisRecord.Data
-		dataText := string(dataBytes)
 
-		fmt.Printf("%s Data = %s \n", record.EventName, dataText)
+		data := kinesisRecord.Data
+
+		if len(data) == 0 {
+			continue
+		}
+
+		group := &model.Group{}
+		err := proto.Unmarshal(data, group)
+		if err != nil {
+			return fmt.Errorf("Failed to unmarshal event: %v", err)
+		}
+
+		fmt.Println("record handled")
 	}
 
 	return nil
